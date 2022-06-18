aa825584091705c54247716a547df2293b5d550e
Frauxsis <Radffodayuan@gmail.com>
2022-06-18 19:57:32 +0800

    20220618

--- react_scaffold/src/App.jsx
+++ react_scaffold/src/App.jsx
@@ -1,16 +1,9 @@
-//创建外壳组件App
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Hello from './components/Hello';
-import Welcome from './components/Welcome';
-//创建并暴露App组件
 export default class App extends Component {
-	render() {
-		return (
-			<div>
-				<Hello />
-				<Welcome />
-			</div>
-		);
-	}
+  render() {
+	 return (
+		<div>App</div>
+	 )
+  }
 }
